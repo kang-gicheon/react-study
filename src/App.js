53dc01872574b7d17eb03b988d0eaada1540a022
kanggicheon <snoopy1516@naver.com>
2023-06-08 10:41:04 +0900

    create 기능 최종 완성

--- src/App.js
+++ src/App.js
@@ -46,8 +46,9 @@ function Create(props){
       <form onSubmit={event=>{
         event.preventDefault();
         const title = event.target.title.value;
-        const body = event.target.body.value;
-        props.onCreate(title,body);
+        const body =  event.target.body.value;
+        props.onCreate(title, body);
+
       }}>
         <p><input type="text" name="title" placeholder='title' /></p>
         <p><textarea name="body" placeholder='body'></textarea></p>
@@ -57,16 +58,44 @@ function Create(props){
   )  
 }
 
+function Update(props){
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return( 
+  <article>
+    <h2>Update</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body =  event.target.body.value;
+      props.onUpdate(title, body);
+
+    }}>
+      <p><input type="text" name="title" placeholder='title' value={title} onChange={event=>{
+          setTitle(event.target.value);
+      }}/></p>
+      <p><textarea name="body" placeholder='body' value={body} onChange={event=>{
+          setBody(event.target.value);
+      }}></textarea></p>
+        
+      <p><input type="submit" value="Update"></input></p>
+    </form>
+  </article>
+)}
+
+
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const[id, setId] = useState(null);
-  const [nextId, setNextId] = useState(4);
-  const [topics,setTopics] = useState([
+  const[nextId, setNextId]  = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
   ]);
+
   let content = null;
+  let contextControl = null;
   if(mode ==='WELCOME'){
     content = <Article title="Welcome" body="hello, Web"></Article>      
   }else if(mode === 'READ'){
@@ -78,20 +107,46 @@ function App() {
         body =  topics[i].body;
       }
     }
-    content = <Article title={title} body={body}></Article>      
+    content = <Article title={title} body={body}></Article>
+    contextControl = <li><a href={'/update/' + id} onClick={event=>{
+      event.preventDefault();
+      setMode('UPDATE');
+    }}>Update</a></li>   
   }else if(mode === 'CREATE'){
-    content = <Create onCreate = {(_title,_body)=>{
-      const newTopic = {id:nextId,title:_title, body:_body}
-      const newTopics = [...topics]
+    content = <Create onCreate = {(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics =[...topics]
       newTopics.push(newTopic);
-      setTopics(newTopics);
+      setTopics(newTopics);  
       setMode('READ');
       setId(nextId);
       setNextId(nextId+1);
-
     }}></Create>
   }else if(mode === 'UPDATE'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      console.log(topics[i].id, id);
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body =  topics[i].body;
+      }
+    }
 
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+    console.log(title, body);
+    const newTopics = [...topics];
+    const updatedTopic = {id:id, title:title, body:body}
+
+      for(let i = 0 ; i < newTopics.length ; i++) {
+        if(newTopics[i].id === id) {
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+
+      setTopics(newTopics);
+
+    }}></Update>
   }
 
   return (
@@ -104,10 +159,14 @@ function App() {
           setId(_id);
        }}></Nav>
       {content}
-      <a href="/create" onClick={event=>{
-        event.preventDefault();
-        setMode('CREATE');
-      }}>Create</a>
+      <ul>
+       <li><a href="/create" onClick={event=>{
+          event.preventDefault();
+          setMode('CREATE');
+          }}>Create</a>
+        </li>
+          {contextControl}
+      </ul>
     </div>
   );
 }
